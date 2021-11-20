296e024bf282458da8b2342c0d009be9d15a89ca
Shin Yi <contact.shinyi@gmail.com>
2021-11-20 10:55:00 +0800

    added curosor style

--- src/components/ResultContainer.js
+++ src/components/ResultContainer.js
@@ -15,14 +15,14 @@ const ResultContainer = ({
     <div className="container">
       <div className="result">
         <img src={icon} alt={text} />
-        <h1>{country}</h1>
-        <h2>{text}</h2>
+        <h1 className="cursornone">{country}</h1>
+        <h2 className="cursornone">{text}</h2>
         <br />
-        <h2 className="nobold">temperature: {temp}°C</h2>
-        <h2 className="nobold">cloud: {cloud}%</h2>
-        <h2 className="nobold">uv index: {uv}</h2>
-        <h2 className="nobold">humidity: {humidity}%</h2>
-        <h2 className="nobold">wind speed: {wind}kph</h2>
+        <h2 className="nobold cursornone">temperature: {temp}°C</h2>
+        <h2 className="nobold cursornone">cloud: {cloud}%</h2>
+        <h2 className="nobold cursornone">uv index: {uv}</h2>
+        <h2 className="nobold cursornone">humidity: {humidity}%</h2>
+        <h2 className="nobold cursornone">wind speed: {wind}kph</h2>
         <br />
       </div>
       <Link to="/currentweather">
